01108e83bca81c7041d26ad4baeddb1db9fe4319
kousaba <kousaba1@proton.me>
2026-01-16 19:36:28 +0900

    feat: textureフォルダを追加

--- src/main/java/com/nozomin/factorism/block/ModBlocks.java
+++ src/main/java/com/nozomin/factorism/block/ModBlocks.java
@@ -6,6 +6,8 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredHolder;
